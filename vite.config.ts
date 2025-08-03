fc8afb0220863b39570039541eb64249501c9c59
Willem Helmet Pickleman <willemhelmet@gmail.com>
2025-08-03 12:44:49 -0400

    pedrotech: push to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/crdt-voxels/",
   plugins: [wasm(), react()],
+  base: "/crdt-voxels/",
 });
